a16ee21aeece53bf515549f9601516e4c7c6b0a3
guyuexing <neu_zhuhao@163.com>
2017-04-19 22:40:57 +0800

    初始化项目

---
+++ DataStruct/DataStruct/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  DataStruct
+//
+//  Created by guyuexing on 17/4/19.
+//  Copyright © 2017年 neu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
