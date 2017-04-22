d4175c9b3196826807e1b84eae7e066825c4f6ab
guyuexing <neu_zhuhao@163.com>
2017-04-22 16:04:16 +0800

    基本的函数说明

--- DataStruct/DataStruct/PublicHeader.h
+++ DataStruct/DataStruct/PublicHeader.h
@@ -20,7 +20,20 @@
 #define OK 1
 #define ERROR 0
 #define INFEASIBLE -1
+#define OVERFLOW -2
 
 typedef int Status;  // Status是函数的类型,其值是函数结果状态代码，如OK等
 typedef int Boolean; // Boolean是布尔类型,其值是TRUE或FALSE
+
+/**
+ *  基本的函数说明
+ 
+    求最大值        max(表达式1,...,表达式n)
+    求最小值        min(表达式1,...,表达式n)
+    求绝对值        abs(表达式)
+    求不足整数值     floor(表达式)
+    求进位整数值     ceil(表达式)
+    判定文件结束     eof(文件变量)或 eof
+    判定行结束       eoln(文件变量)或 eoln
+ */
 
