14f5d73cf2588080f1a34bb4fd04256073af2965
haozhu <hao.zhu@ihandysoft.com>
2017-05-20 17:09:51 +0800

    公开操作方法

--- DataStruct/ch2.线性表/DuLinkList.hpp
+++ DataStruct/ch2.线性表/DuLinkList.hpp
@@ -15,3 +15,34 @@ typedef struct DuLNode{
     DuLNode *next, *prior;
     ElemType data;
 }DuLNode, *DuLinkList;
+
+Status initList(DuLinkList &L);
+
+Status DestoryList(DuLinkList &L);
+
+Status ClearList(DuLinkList L);
+
+Status ListEmpty(DuLinkList L);
+
+int ListLength(DuLinkList L);
+
+Status GetElem(DuLinkList L, int i, ElemType &e);
+
+Status IsEqual(ElemType e1, ElemType e2);
+
+int LocaleElem(DuLinkList L, ElemType e, Status (*compare)(ElemType, ElemType));
+
+Status PriorElem(DuLinkList L, ElemType cur_e, ElemType &prior_e);
+
+Status NextElem(DuLinkList L, ElemType cur_e, ElemType &next_e);
+
+DuLinkList GetElemP(DuLinkList L, int i);
+
+Status ListInsert(DuLinkList L, int i, ElemType e);
+
+Status ListDelete(DuLinkList L, int i, ElemType &e);
+
+void ListTraverse(DuLinkList L, void(*visit)(ElemType e));
+
+void ListTraverseBack(DuLinkList L, void(*visit)(ElemType e));
+
